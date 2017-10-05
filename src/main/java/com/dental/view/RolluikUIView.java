df57e1cb6e829b4ec7c96004d603b71d08853e0e
biprom <bram@biprom.com>
2017-10-05 07:46:57 +0200

    Initial commit

---
+++ src/main/java/com/dental/view/RolluikUIView.java
@@ -0,0 +1,12 @@
+package com.dental.view;
+
+import com.dental.design.RolluikenUIDesign;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+
+public class RolluikUIView extends RolluikenUIDesign implements View{
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+
+    }
+}
